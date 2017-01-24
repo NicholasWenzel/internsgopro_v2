eaa4a9c16867f8c53d66168a5d227074e90081f9
NicholasWenzel <nicholas.wenzel@internsgopro.com>
2017-01-24 16:06:46 +0100

    Placing content for landing page (offers page)

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ end
 
   resources :offers
   resources :places
+  resources :labels
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 get 'offers' => 'offers#index'
