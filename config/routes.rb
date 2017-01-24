51341ffcef23f9f4ccb52aa981caf9b3989da25b
NicholasWenzel <nicholas.wenzel@internsgopro.com>
2017-01-24 17:16:05 +0100

    Placing content for label page (pages)

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ end
 
   resources :offers
   resources :places
-  resources :labels
+  resources :pages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 get 'offers' => 'offers#index'
